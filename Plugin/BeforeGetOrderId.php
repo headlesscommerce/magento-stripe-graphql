5aebf8ef4bf9c9dc4f4cb46cc60f220c54d489ce
David Windell <david@outeredgeuk.com>
2022-11-08 19:33:18 +0000

    Add plugin to reverse calculate cart ID to order ID

---
+++ Plugin/BeforeGetOrderId.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Headless\StripeGraphQl\Plugin;
+
+use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
+use StripeIntegration\Payments\Helper\Webhooks;
+
+class BeforeGetOrderId
+{
+    protected $salesOrderCollectionFactory;
+
+    public function __construct(CollectionFactory $salesOrderCollectionFactory)
+    {
+        $this->salesOrderCollectionFactory = $salesOrderCollectionFactory;
+    }
+
+    /**
+     * Try and convert Cart ID to Order ID
+     */
+    public function beforeGetOrderIdFromObject(Webhooks $subject, array $object, $includeMultishipping = false)
+    {
+        if (!empty($object['description']) && preg_match('/Cart\s([0-9]*)\s/', $object['description'], $matches)) {
+            if ($order = $this->orderCollectionFactory->create()->addFieldToFilter('quote_id', $matches[1])->getFirstItem()) {
+                return $order->getIncrementId();
+            }
+        }
+
+        return null;
+    }
+}
